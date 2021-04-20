da2e4fb870344e92825509ea08025777a00990af
baeksangwoo <qortkdn@naver.com>
2021-04-21 00:06:16 +0900

    4.props & component

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,48 @@
         color="primary"
         dark
         >
-        <v-app-bar-nav-icon/>
-          <v-toolbar-title>
-            Page Title
-          </v-toolbar-title>
+        <v-app-bar-nav-icon @click="drawer=!drawer"/>
+            <site-title :title="title">
+            </site-title>
           <v-spacer></v-spacer>
           <v-btn icon to="/about">
             <v-icon>mdi-magnify</v-icon>
           </v-btn>
         </v-app-bar>
+        <v-navigation-drawer app v-model="drawer">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title">
+                Application
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                subtext
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-navigation-drawer>
         <v-content>
           <router-view/>
         </v-content>
-        <v-footer
-            color="primary"
-            dark
-            absolute>
-            <v-spacer></v-spacer>
-            <div>&copy; {{ new Date().getFullYear() }}</div>
-          </v-footer>
+        <site-footer :footer="footer">
+
+        </site-footer>
     </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
 
 export default {
-  name: 'App'
+  components: { SiteTitle, SiteFooter },
+  name: 'App',
+  data () {
+    return {
+      title: '나의 타이틀입니다.',
+      footer: '푸터입니다',
+      drawer: false
+    }
+  }
 }
 </script>
---
+++ src/views/site/footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-footer
+            color="primary"
+            dark
+            absolute>
+            <v-spacer></v-spacer>
+            <div>&copy; {{ new Date().getFullYear() + ' ' + footer}}</div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+  props: ['footer']
+}
+</script>
